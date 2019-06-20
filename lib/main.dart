1713c0f85d368ca43b6acbdff22ff159b34816e2
Vivek Kekuda <vivekkekuda29@gmail.com>
2019-06-21 01:03:47 +0530

    migrated to androidx and integrated firebase, still have registerResGeneratingTask deprecated warning

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override 
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Take Notes',
+      home: Scaffold(),
+    );
+  }
+}
